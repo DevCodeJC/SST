9cb118085a60ef7f9b2a11db15b94ec4b65a5210
Juan <jcperez16@outok.com>
2023-01-23 20:20:49 -0500

    Version Final

--- includes/Conexion.php
+++ includes/Conexion.php
@@ -1,6 +1,6 @@
 <?php
 // datos para la conexion a mysql
-$cx = mysqli_connect('localhost', 'root', 'T1ck3t3', 'sst'); // utlizar mysqli_connect
+$cx = mysqli_connect('localhost', 'root', 'root', 'sst'); // utlizar mysqli_connect
 $selectDb = mysqli_select_db($cx,"sst"); // mysqli_select_db - primero la conexión, después el nombre de la BD 
 mysqli_set_charset($cx, "utf8");
 ?>
