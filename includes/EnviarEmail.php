9c2d972124f8bb7541f6053f3e4b9ec142d4256d
Juan <jcperez16@outok.com>
2023-01-23 20:32:05 -0500

    Version Final

--- includes/EnviarEmail.php
+++ includes/EnviarEmail.php
@@ -42,8 +42,8 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'prueba@siegfried.com.co';                     // SMTP username
-    $mail->Password   = 'Prueba.2022';                               // SMTP password
+    $mail->Username   = 'pruebas@siegfried.com.co';                     // SMTP username
+    $mail->Password   = 'Pruebas.2022';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
